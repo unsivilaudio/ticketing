db5a5fde3eaf7255407311aede903f29c579e492
Joel Jacobs <unsivilaudio@gmail.com>
2022-04-22 08:04:45 -0600

    broken code

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -64,7 +64,7 @@ ticketSchema.statics.build = (attrs: TicketAttrs) => {
 
 ticketSchema.methods.isReserved = async function () {
     const existingOrder = await Order.findOne({
-        ticket: this,
+        ticket: this as TicketDoc,
         status: {
             $in: [
                 OrderStatus.Created,
